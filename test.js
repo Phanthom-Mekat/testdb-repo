db4fb12ecae5c72febdddde06c5485d90d00dc45
Mustaqueem <131261010+MustaqueemAlam@users.noreply.github.com>
2024-07-15 23:39:48 +0600

    Update test.js

--- test.js
+++ test.js
@@ -1,19 +1,20 @@
 const mysql = require('mysql2');
 
-const pool =mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'mekat',
-    database:'testdb',
+const pool = mysql.createPool({
+    host: 'sql12.freesqldatabase.com',
+    user: 'sql12719594',
+    password: 'GHYRx2vI2f',
+    database: 'sql12719594',
+    port: 3306,
     connectionLimit: 10
 });
 
-// pool.query('select * from tbl_review', (err, results, fields) => {
-//     if(err){
-//         return console.log(err.message);
-//     }
-//     return console.log(results);
-// });
+pool.query('select * from tbl_review', (err, results, fields) => {
+   if(err){
+       return console.log(err.message);
+    }
+     return console.log(results);
+});
 
 // const ReviewID = 60000008;
 // pool.query('select * from tbl_review where ReviewID=? ',[ReviewID], (err, results, fields) => {
